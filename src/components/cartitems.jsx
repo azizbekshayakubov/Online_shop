56fe64283e5ec267748906d75e69ad6fa39623c4
Azizbek Shoyoqubov <a.shoyoqubov@evos.uz>
2023-11-27 18:10:59 +0500

    style cartitems components

--- src/components/cartitems.jsx
+++ src/components/cartitems.jsx
@@ -1,10 +1,15 @@
-/* eslint-disable react/prop-types */
 import { useContext } from "react";
 import { ShopContext } from "./shopcontext";
 
 const Cartitems = (props) => {
   const { id, name, image, price, brand } = props.data;
-  const { viewProductDetails, addToCart, cartItems } = useContext(ShopContext);
+  const {
+    viewProductDetails,
+    addToCart,
+    cartItems,
+    removeToCart,
+    updateCartItemCount,
+  } = useContext(ShopContext);
   return (
     <>
       <section className="cart-item p-5">
@@ -20,12 +25,57 @@ const Cartitems = (props) => {
               <tbody>
                 <tr>
                   <td>
-                    <img src={image} alt="" className="img-fluid" />
+                    <div className="col-3">
+                      <img src={image} alt="" className="img-fluid" />
+                    </div>
                   </td>
                   <td>
                     <p>{brand}</p>
                     <p>{name}</p>
                     <p>{price}</p>
+                  </td>
+                  <td>
+                    <button
+                      className="add-btn"
+                      onClick={() => {
+                        addToCart(id);
+                        event.target.classlist.toggle("red");
+                      }}
+                    >
+                      +
+                    </button>
+                    <input
+                      type="text"
+                      value={cartItems[id]}
+                      onChange={(e) =>
+                        updateCartItemCount(Number(e.target.value), id)
+                      }
+                    />
+                  </td>
+                  <td>
+                    <button
+                      className="add-btn"
+                      onClick={() => {
+                        removeToCart(id);
+                        event.target.classlist.toggle("red");
+                      }}
+                    >
+                      -
+                    </button>
+                  </td>
+                  <td>
+                    <div className="input-group mb-3">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Recipient's username"
+                        aria-label="Recipient's username"
+                        aria-describedby="basic-addon2"
+                      />
+                      <span className="input-group-text" id="basic-addon2">
+                        Search
+                      </span>
+                    </div>
                   </td>
                 </tr>
               </tbody>
