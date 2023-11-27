a1aa039c97242bab6d0154665b55d10cf2010a52
Azizbek Shoyoqubov <a.shoyoqubov@evos.uz>
2023-11-27 15:54:00 +0500

    added addodproduct

---
+++ src/components/cartitems.jsx
@@ -0,0 +1,5 @@
+const Cartitems = () => {
+  return <div>cartitems</div>;
+};
+
+export default Cartitems;
--- src/components/prod.jsx
+++ src/components/prod.jsx
@@ -1,16 +1,57 @@
+import { useContext } from "react";
 import { ShopContext } from "./shopcontext";
+import ReactStars from "react-rating-stars-component";
+import { Link } from "react-router-dom";
 
 const Prod = (props) => {
   const { id, name, image, price, brand } = props.data;
-
+  const { viewProductDetails, addToCart, cartItems } = useContext(ShopContext);
+  const cartItemCount = cartItems[id];
   return (
     <>
-      <div key={id} className="col-3">
+      <div key={id} className="col-3 mb-4">
         <div className="card">
-          <img src={image} alt="" className="img-fluid card-img-top" />
-          <div className="card-details p-2">
+          <div className="p-2">
+            <img src={image} alt="" className="img-fluid card-img-top" />
+          </div>
+          <div className="card-details p-3">
             <span>{brand}</span>
-            <h5 className="my-3">{name}</h5>
+            <h5 className="my-2">{name}</h5>
+            <ReactStars
+              count={5}
+              edit={false}
+              value={4}
+              size={24}
+              activeColor="#cca27e"
+            />
+            <p className="price my-2">
+              {price}
+              <span className="text-danger">
+                &nbsp;
+                <strike>{price * 2}</strike>
+              </span>
+            </p>
+            <div className="d-flex align-items-center justify-content-around mb-2">
+              <div className="col-6 d-flex align-items-center justify-content-around">
+                <Link
+                  className="view-button"
+                  onClick={() => viewProductDetails(id)}
+                >
+                  View Details
+                </Link>
+              </div>
+              <div className="col-6 d-flex align-items-center justify-content-around">
+                <button
+                  className="add-btn"
+                  onClick={() => {
+                    addToCart(id);
+                    event.target.classlist.toggle("red");
+                  }}
+                >
+                  Add To Cart{cartItemCount > 0 && `(${cartItemCount})`}
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
