5154a58f09a1b632c8cd7ee2d3fe981580d65d81
Azizbek Shoyoqubov <a.shoyoqubov@evos.uz>
2023-11-27 16:24:25 +0500

    added cartitems components

--- src/components/cartitems.jsx
+++ src/components/cartitems.jsx
@@ -1,5 +1,40 @@
-const Cartitems = () => {
-  return <div>cartitems</div>;
+/* eslint-disable react/prop-types */
+import { useContext } from "react";
+import { ShopContext } from "./shopcontext";
+
+const Cartitems = (props) => {
+  const { id, name, image, price, brand } = props.data;
+  const { viewProductDetails, addToCart, cartItems } = useContext(ShopContext);
+  return (
+    <>
+      <section className="cart-item p-5">
+        <div className="container-xxl">
+          <div key={id} className="row">
+            <table>
+              <thead>
+                <th>Product Image</th>
+                <th>Product Details</th>
+                <th>Edit</th>
+                <th>Coupons</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <img src={image} alt="" className="img-fluid" />
+                  </td>
+                  <td>
+                    <p>{brand}</p>
+                    <p>{name}</p>
+                    <p>{price}</p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Cartitems;
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,5 +1,22 @@
+import { PRODUCTS, PRODUCTS1 } from "../components/products";
+import Cartitems from "../components/cartitems";
+import { useContext } from "react";
+import { ShopContext } from "../components/shopcontext";
+
 const Cart = () => {
-  return <div>cart</div>;
+  const { viewProductDetails, addToCart, cartItems } = useContext(ShopContext);
+  return (
+    <>
+      <div className="row">
+        {[...PRODUCTS, ...PRODUCTS1].map((product) => {
+          if (Cartitems[product.id] !== 0) {
+            return <Cartitems key={product.id} data={product} />;
+          }
+        })}
+      </div>
+      ;
+    </>
+  );
 };
 
 export default Cart;
