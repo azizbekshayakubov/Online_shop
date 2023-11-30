a2ca9ced942fea4c5a00e453a789900bd0192feb
Azizbek Shoyoqubov <a.shoyoqubov@evos.uz>
2023-11-30 16:46:01 +0500

    added style cart page

--- src/components/cartitems.jsx
+++ src/components/cartitems.jsx
@@ -12,77 +12,58 @@ const Cartitems = (props) => {
   } = useContext(ShopContext);
   return (
     <>
-      <section className="cart-item p-5">
-        <div className="container-xxl">
-          <div key={id} className="row">
-            <table>
-              <thead>
-                <th>Product Image</th>
-                <th>Product Details</th>
-                <th>Edit</th>
-                <th>Coupons</th>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <div className="col-3">
-                      <img src={image} alt="" className="img-fluid" />
-                    </div>
-                  </td>
-                  <td>
-                    <p>{brand}</p>
-                    <p>{name}</p>
-                    <p>{price}</p>
-                  </td>
-                  <td>
-                    <button
-                      className="add-btn"
-                      onClick={() => {
-                        addToCart(id);
-                        event.target.classlist.toggle("red");
-                      }}
-                    >
-                      +
-                    </button>
-                    <input
-                      type="text"
-                      value={cartItems[id]}
-                      onChange={(e) =>
-                        updateCartItemCount(Number(e.target.value), id)
-                      }
-                    />
-                  </td>
-                  <td>
-                    <button
-                      className="add-btn"
-                      onClick={() => {
-                        removeToCart(id);
-                        event.target.classlist.toggle("red");
-                      }}
-                    >
-                      -
-                    </button>
-                  </td>
-                  <td>
-                    <div className="input-group mb-3">
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Recipient's username"
-                        aria-label="Recipient's username"
-                        aria-describedby="basic-addon2"
-                      />
-                      <span className="input-group-text" id="basic-addon2">
-                        Search
-                      </span>
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </section>
+      <tbody key={id}>
+        <tr>
+          <td className="col-3">
+            <img src={image} alt="" className="img-fluid" />
+          </td>
+          <td className="col-3">
+            <p>{brand}</p>
+            <p>{name}</p>
+            <p>{price}</p>
+          </td>
+          <td>
+            <button
+              className="add-btn"
+              onClick={() => {
+                addToCart(id);
+              }}
+            >
+              +
+            </button>
+            <input
+              type="text"
+              value={cartItems[id]}
+              onChange={(e) => updateCartItemCount(Number(e.target.value), id)}
+            />
+            <button
+              className="add-btn"
+              onClick={() => {
+                removeToCart(id);
+                event.target.classlist.toggle("red");
+              }}
+            >
+              -
+            </button>
+          </td>
+
+          <td>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter coupon code ..."
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <span className="input-group-text" id="basic-addon2">
+                Search
+              </span>
+            </div>
+          </td>
+        </tr>
+        <div className="mb-2"></div>
+      </tbody>
     </>
   );
 };
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -7,14 +7,26 @@ const Cart = () => {
   const { viewProductDetails, addToCart, cartItems } = useContext(ShopContext);
   return (
     <>
-      <div className="row">
-        {[...PRODUCTS, ...PRODUCTS1].map((product) => {
-          if (Cartitems[product.id] !== 0) {
-            return <Cartitems key={product.id} data={product} />;
-          }
-        })}
-      </div>
-      ;
+      <section className="cart-item p-5">
+        <div className="container-xxl">
+          <div className="row">
+            <table>
+              <thead className="my-2">
+                <th className="col-3">Product Image</th>
+                <th className="col-3">Product Details</th>
+                <th className="col-3">Edit</th>
+                <th className="col-3">Coupons</th>
+              </thead>
+
+              {[...PRODUCTS, ...PRODUCTS1].map((product) => {
+                if (cartItems[product.id] !== 0) {
+                  return <Cartitems key={product.id} data={product} />;
+                }
+              })}
+            </table>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
